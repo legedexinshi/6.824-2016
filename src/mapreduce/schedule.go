eee73eb394b8132c8c4087ff3cbb2e389d0b02f0
ztl <zhoutingle@gmail.com>
2017-03-20 15:43:55 +0800

    project init

---
+++ src/mapreduce/schedule.go
@@ -0,0 +1,62 @@
+package mapreduce
+
+import "fmt"
+
+
+// schedule starts and waits for all tasks in the given phase (Map or Reduce).
+func (mr *Master) schedule(phase jobPhase) {
+	var ntasks int
+	var nios int // number of inputs (for reduce) or outputs (for map)
+	switch phase {
+	case mapPhase:
+		ntasks = len(mr.files)
+		nios = mr.nReduce
+
+	case reducePhase:
+		ntasks = mr.nReduce
+		nios = len(mr.files)
+
+	}
+
+	fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, nios)
+
+	// All ntasks tasks have to be scheduled on workers, and only once all of
+	// them have been completed successfully should the function return.
+	// Remember that workers may fail, and that any given worker may finish
+	// multiple tasks.
+	//
+	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
+	//
+
+	ok := make(chan int, ntasks)
+	for i := 0; i < ntasks; i++ {
+		go func(taskNum int, phase jobPhase, nios int) {
+			for {
+				wk := <- mr.registerChannel
+				args := new(DoTaskArgs)
+				args.JobName = mr.jobName
+				args.Phase = phase
+				args.TaskNumber = taskNum
+				if phase == mapPhase {
+					args.File = mr.files[taskNum]	
+				}
+				args.NumOtherPhase = nios
+				suc := call(wk, "Worker.DoTask", &args, new(struct{}))
+				if suc {
+					go func () {
+						ok <- 1
+						mr.registerChannel <- wk
+					}()
+					break
+				}else {
+					//mr.registerChannel <- wk
+				}
+			}
+		}(i, phase, nios)
+	}
+	for i := 0; i < ntasks; i++ {
+		<- ok
+	}
+
+	fmt.Printf("Schedule: %v phase done\n", phase)
+}
